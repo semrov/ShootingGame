a9803d96bd9103b7fcc0f4e7ddf7f524e3e44d1a
Jure <jure.semrov@outlook.com>
2018-03-06 20:39:09 +0100

    Initial commit

---
+++ app/src/main/java/com/jure/semrov/shootinggame/ShootingGame.java
@@ -0,0 +1,13 @@
+package com.jure.semrov.shootinggame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShootingGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shooting_game);
+    }
+}
